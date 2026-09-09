eadac2cdf186c5a0696393c827261bf39a36bd79
Henrik Busengdal <henrik-gb@hotmail.com>
2026-09-09 17:02:07 +0200

    feat(frontend): initialize frontend using React with Vite and Typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>NCS Explorer</h1>
+          <h4>Explore oil and gas data from the Norwegian Continental Shelf</h4>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
